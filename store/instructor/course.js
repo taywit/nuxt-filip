61d4ec888bb356b3e6c15d321afa3d05330555cf
unknown <vetaom@gmail.com>
2020-04-22 10:47:32 +0700

    สร้างแบบฟอร์มการใส่ข้อมูลยน เตรียมลงข้อมูล

---
+++ store/instructor/course.js
@@ -0,0 +1,34 @@
+
+export const state = () => ({
+  items: [],
+  // item: {},
+  // canUpdateCourse: false
+})
+export const actions ={
+  fetchInstructorCourses({commit}){
+    return this.$axios.$get('/api/v1/products/user-products')
+    .then((courses) =>{
+      commit('setCourses', courses)
+      return state.items
+    })
+    .catch((error) => Promise.reject(error))
+  },
+  createCourse(_, courseData) {
+    return this.$axios.$post('/api/v1/products', courseData)
+  }
+}
+export const mutations = {
+  setCourses(state,courses){
+    state.items = courses
+  },
+}
+
+
+
+
+
+
+
+
+
+
